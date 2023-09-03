02270dba8003f52c2c7fca3a2b2a6edd5678b1e3
EagleDuarte <leonardoduarte074@hotmail.com>
2023-09-03 14:37:25 -0300

    standard tasks

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask (){
-    if(inputBox.value === ''){
-        alert("Escreva algo!")
-    }
-    else{
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Escreva algo!");
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
+        span.addEventListener("click", function () {
+            li.remove();
+            saveData();
+        });
     }
     inputBox.value = "";
     saveData();
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
-    }
-    else if(e.target.tagName = "SPAN"){
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
@@ -37,7 +39,30 @@ function saveData() {
     }
 }
 
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
+function showTask() {
+    const savedData = localStorage.getItem("data");
+    if (savedData) {
+        listContainer.innerHTML = savedData;
+    } else {
+        // Se o localStorage estiver vazio, adicione as tarefas padrão com a opção de exclusão
+        listContainer.innerHTML = `
+            <li>Documento de Identificação <span class="delete">&#x00D7;</span></li>
+            <li>Comprovante de residência (prazo de 90 dias)<br> ou conforme descrito no Fluid <span class="delete">&#x00D7;</span></li>
+            <li>Comprovante de renda (prazo de 60 dias) ou presumida/informada<span class="delete">&#x00D7;</span></li>
+            <li>Imposto de renda - somente sendo referente a 2022/2023<span class="delete">&#x00D7;</span></li>
+            <li>Conferência de dados, valores de crédito e <br> cheque antes de efetivação<span class="delete">&#x00D7;</span></li>
+        `;
+
+        // Adicione eventos de clique para exclusão às tarefas padrão
+        const deleteButtons = listContainer.querySelectorAll(".delete");
+        deleteButtons.forEach(function (button) {
+            button.addEventListener("click", function () {
+                button.parentElement.remove();
+                saveData();
+            });
+        });
+    }
 }
+
+// Chame a função showTask() após a definição dos eventos
 showTask();
